218f7b960bf7148f9087e11c46d0ec84fe41514d
Dawn Treader <69836062+alex-stew@users.noreply.github.com>
2021-05-25 13:05:20 +0930

    wip views

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create();
+
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
@@ -32,5 +34,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening on' PORT));
+  app.listen(PORT, () => console.log('Now listening on: PORT ' + PORT));
 });
